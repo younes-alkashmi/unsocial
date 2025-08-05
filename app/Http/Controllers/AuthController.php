bbb6909418e47043177ed0a79aabf4023f5c2154
alaqouri <younus.alaqouri@gmail.com>
2025-08-05 20:00:51 +0200

    token_type added to login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,6 +28,7 @@ class AuthController extends Controller
 
         return response()->json([
             'token' => $token,
+            'token_type' => 'Bearer',
             'user' => $user
         ]);
     }
